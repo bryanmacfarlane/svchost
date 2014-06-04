c1eaf8ee4efcd742318294c4a3835202016b9b47
Bryan MacFarlane <bryanmacf@gmail.com>
2014-06-03 22:36:08 -0400

    customize restart logic

--- samples/badserver.js
+++ samples/badserver.js
@@ -1,18 +1,21 @@
-function randomCrash (res) {
-    if (Math.floor(Math.random() * 3 % 3) == 0) {
-    	throw new Error('Oops, our server crashed');
-    }
-}
+var counter = 0;
 
 var http = require('http');
 http.createServer(function (req, res) {
-  randomCrash(res);
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+    return;
+  }
+
+  // crash every other request
+  counter++;
+  if (counter % 2 == 0) {
+  		throw new Error('Oops, our server crashed');
+  }
+
+  console.log('request processed');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
-
-process.on('uncaughtException', function(err) {
-	console.error('Caught exception: ' + err);
-	process.exit(1);
-});
--- samples/runme.js
+++ samples/runme.js
@@ -1,12 +1,12 @@
 var sh = require('../lib/svchost');
 
 var host = new sh.SvcHost();
-host.on('start', function(pid){
-	console.log('started (' + pid + ')');
+host.on('start', function(pid, starts){
+	console.log('started (' + pid + ') - ' + starts + ' starts');
 });
 
-host.on('restart', function(starts){
-	console.log('restart.  ' + starts + ' starts.');
+host.on('restart', function(){
+	console.log('restart.  ');
 });		
 
 host.on('exit', function(code, reason){
@@ -17,6 +17,29 @@ host.on('abort', function(){
 	console.log('abort after restarts');
 });		
 
+//
+// api consumer can control restarts, sleeps, grow etc...
+// this example allows 5 starts with a wait of a second per restart in between
+//
+var maxStarts = 5;
+var RESTART_DELAY = 1000;  // 1 sec
+
+var handleRestart = function(starts, relaunch) {
+	console.log(starts, 'starts');
+	if (starts < maxStarts) {
+		console.log('waiting to restart');
+		setTimeout(function(){
+				relaunch(true);
+			}, 
+			RESTART_DELAY*starts);			
+	} 
+	else {
+		console.log('fail');
+		relaunch(false);
+	}
+}
+
 host.start('./badserver.js',           // app.  relative or absolute path
 			[],                        // args 
-			{outputPath: __dirname});  // optional.  defaults to apps directory
+			{outputPath: __dirname},   // optional.  defaults to apps directory
+			handleRestart);  
